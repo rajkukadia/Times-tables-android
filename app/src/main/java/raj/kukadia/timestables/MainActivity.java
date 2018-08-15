10003934a99c826064cc0b8a9a44ab9d1076061e
Raj <rajkukadia1@gmail.com>
2018-08-14 20:46:18 -0400

    seekbar and multiplication

--- app/src/main/java/raj/kukadia/timestables/MainActivity.java
+++ app/src/main/java/raj/kukadia/timestables/MainActivity.java
@@ -2,7 +2,12 @@ package raj.kukadia.timestables;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SeekBar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView listView = findViewById(R.id.list);
+        final ListView listView = findViewById(R.id.list);
+        final ArrayList<Integer> arrayList = new ArrayList<>();
 
+        final ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(adapter);
+        SeekBar seekBar = findViewById(R.id.slider);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Log.d("Seek value", String.valueOf(i));
+                    adapter.clear();
+                for(int k = 1; k<10; k++){
+                 arrayList.add(i*k);
+             }
+                listView.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
